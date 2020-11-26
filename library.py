4cd5f4247beed5d03aa8f042e8878c8199036e5a
NikKrek <35462651+NikKrek@users.noreply.github.com>
2020-11-26 14:34:32 +0200

    Update library.py

--- library.py
+++ library.py
@@ -1,86 +1,112 @@
+'''
+1. Класс библиотека
+    Поля:
+        - список книг (list Book)
+        - список должников (list People)
 
+    Методы:
+        - Добавить книгу
+        - Удалить книгу
+        - Отдать книгу посетителю
+        - Принять книгу от посетителя
+
+        - Вывести список всех книг
+        - Вывести список книг в библиотеке (в наличии)
+        - Вывести списк книг, которые не в наличии
+
+        - Отсортировать список книг по названию, автору, году издания (lambda будет плюсом)
+
+2. Класс книга
+    Поля:
+        - ID
+        - Название
+        - Автор
+        - Год издания
+        - ??? (этот параметр нужен!!!)
+
+3. Класс People
+    - ???
+
+    Методы:
+        ???
+'''
 
 class SuBook:
 
-    def __init__(self, name, author, year, idB, borrow=None):
+    def __init__(self, name, author, year, id_b, borrow=None):
 
         self.name = name
         self.author = author
         self.year = year
-        self.idB = idB
+        self.id_b = id_b
         self.borrow = borrow
 
     def __repr__(self):
-        return f'{self.name},{self.author},{self.year},{self.idB}'
+        return f'{self.name},{self.author},{self.year},{self.id_b}'
 
     def __str__(self):
-        return f'{self.name},{self.author},{self.year},{self.idB}'
-
+        return f'{self.name},{self.author},{self.year},{self.id_b}'
 
 
 class SuLibrary:
-    def __init__(self, listBook:list):
-        self.listBook = listBook
+    def __init__(self, list_book: list, list_man):
+        self.list_book = list_book
+        self.list_man = list_man
 
-    def __repr__(self):
-        return f'{self.listBook}'
+    def add_book(self, add_book):
+        self.list_book.append(add_book)
 
-    def __str__(self):
-        return f'{self.listBook}'
-
-    def addBook(self, addBook):
-        self.listBook.append(addBook)
-
-    def removeBook(self, idRemoveBook:int):
+    def remove_book(self, id_remove_book: int):
         j = 1
-        for i in self.listBook:
-        
-            if idRemoveBook == j:
-                self.listBook.remove(i)
+        for i in self.list_book:
+            if id_remove_book == j:
+                self.list_book.remove(i)
             j += 1
 
-    def printAllBooks(self):
-        book = self.listBook
+    def print_all_books(self):
+        book = self.list_book
         print('%-50s%-20s%-5s%-2s' % ('Name', 'Author', 'Year', 'id'))
-        for ij in book:
-            bookT = str(ij)
-            bookT = bookT.replace("[", "")
-            bookT = bookT.replace("]", "")
-            bookT = bookT.split(',')
-            print('%-50s%-20s%-5s%-2s' % (bookT[0], bookT[1], bookT[2], bookT[3]))
-    # def giveBook(self):
-        
+        for i in book:
+            book_temp = str(i)
+            book_temp = book_temp.replace("[", "")
+            book_temp = book_temp.replace("]", "")
+            book_temp = book_temp.split(',')
+            print('%-50s%-20s%-5s%-2s' % (book_temp[0], book_temp[1], book_temp[2], book_temp[3]))
+
+    def give_book(self, id_book, id_man):
+        print(id_book)
+        print(id_man)  
+          
 
 
 class SuPeople:
-    def __init__(self, name, surname, age, debt=None):
+    def __init__(self, name, surname, id_p, debt=None):
         self.name = name
         self.surname = surname
-        self.age = age
+        self.id_p = id_p
         self.debt = debt
 
-
     def __repr__(self):
-        return f'{self.name},{self.surname},{self.age}'
+        return f'{self.name},{self.surname},{self.id_p}'
 
     def __str__(self):
-        return f'{self.name},{self.surname},{self.age}'
+        return f'{self.name},{self.surname},{self.id_p}'
 
 
 if __name__ == "__main__":
-    book1 = SuBook(name='Harry Potter & blah blah', author='Rolling', year=1995, idB=1)
-    book2 = SuBook(name='Charley & chocolate factory ', author='Dahll', year=1970, idB=2)
+    book1 = SuBook(name='Harry Potter & blah blah', author='Rolling', year=1995, id_b=1)
+    book2 = SuBook(name='Charley & chocolate factory ', author='Dahll', year=1970, id_b=2)
 
     myLib = SuLibrary([book1, book2])
 
-    man1 = SuPeople(name='Bob', surname='Stragolsky', age=37)
+    man1 = SuPeople(name='Bob', surname='Stragolsky', id_p=1)
 
-    book3 = SuBook(name='TestTestTest', author='TestTest', year=1980, idB=3)
+    book3 = SuBook(name='TestTestTest', author='TestTest', year=1980, id_b=3)
 
-    myLib.addBook(book3)
+    myLib.add_book(book3)
 
-    # SuLibrary.printAllBooks(myLib)
+    myLib.remove_book(3)
 
-    myLib.removeBook(3)
+    SuLibrary.print_all_books(myLib)
 
-    SuLibrary.printAllBooks(myLib)
+    SuLibrary.give_book(book2, man1)
