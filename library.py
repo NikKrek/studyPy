da4ce94e9cf3d4c37a7828632ed3ec4fafda585f
NikKrek <35462651+NikKrek@users.noreply.github.com>
2020-12-01 19:13:02 +0200

    Update library.py

--- library.py
+++ library.py
@@ -32,25 +32,28 @@
 '''
 
 
+import tkinter
+
+
+
+
 class SuBook:
 
-    def __init__(self, name, author, year, id_b, borrow=False):
-
+    def __init__(self, name, author, year, id_b):
         self.name = name
         self.author = author
         self.year = year
         self.id_b = id_b
-        self.borrow = borrow
 
     def __repr__(self):
-        return f'{self.name},{self.author},{self.year},{self.id_b},{self.borrow}'
+        return f'{self.name},{self.author},{self.year},{self.id_b}'
 
     def __str__(self):
-        return f'{self.name},{self.author},{self.year},{self.id_b},{self.borrow}'
+        return f'{self.name},{self.author},{self.year},{self.id_b}'
 
 
 class SuLibrary:
-    def __init__(self, list_book: list, man=None):
+    def __init__(self, list_book: list, man: list):
         self.list_book = list_book
         self.man = man
 
@@ -64,63 +67,61 @@ class SuLibrary:
                 self.list_book.remove(i)
             j += 1
 
-    def print_all_books(self):
+    def print_lib_books(self):
         book = self.list_book
-        print('%-50s%-20s%-5s%-2s' % ('Name', 'Author', 'Year', 'id'))
+        print(self.list_book)
+        print('%40s' % 'Shelf of library')
+        print('%-50s%-20s%-5s%-3s' % ('Name:', 'Author', 'Year', 'id'))
         for i in book:
             book_temp = str(i)
             book_temp = book_temp.replace("[", "")
             book_temp = book_temp.replace("]", "")
             book_temp = book_temp.split(',')
-            print('%-50s%-20s%-5s%-2s' % (book_temp[0], book_temp[1], book_temp[2], book_temp[3]))
+            print(f'{book_temp[0]:50}{book_temp[1]:20}{book_temp[2]:5}{book_temp[3]:3}')
 
-    def give_book(self, id_book: int, id_man: int):
-        for i in self.list_book:
-            print(i)
-            j = str(i)
-            j = j.split(',')
-            print(j[4])
-            # if j[3] == id_book and j[4] == 'False':
-            #     SuLibrary.remove_book(id_book)
+    def give_book(self, mans, books):
+        if books in self.list_book:
+            mans.debt.append(books)
+            books = str(books)
+            books = books.split(',')
+            j = books[3]
+            self.remove_book(int(j))
 
-
-            print(i[3])
-
-
-
-
-
-
-
+    def take_book(self, mans, books):
+        if books in mans.debt:
+            mans.debt.remove(books)
+            self.add_book(books)
 
 class SuPeople:
-    def __init__(self, name, surname, id_p, debt=None):
+    def __init__(self, name, surname, id_p, debt=[]):
         self.name = name
         self.surname = surname
         self.id_p = id_p
         self.debt = debt
 
     def __repr__(self):
-        return f'{self.name},{self.surname},{self.id_p}'
+        return f'{self.name},{self.surname},{self.id_p},{self.debt}'
 
     def __str__(self):
-        return f'{self.name},{self.surname},{self.id_p}'
+        return f'{self.name},{self.surname},{self.id_p},{self.debt}'
+
 
 
 if __name__ == "__main__":
     book1 = SuBook(name='Harry Potter & blah blah', author='Rolling', year=1995, id_b=1)
     book2 = SuBook(name='Charley & chocolate factory ', author='Dahll', year=1970, id_b=2)
 
-    myLib = SuLibrary([book1, book2])
-
-    man1 = SuPeople(name='Bob', surname='Stragolsky', id_p=1)
-
+    man1 = SuPeople(name='Rob', surname='Sapolsky', id_p=1)
+    myLib = SuLibrary([book1, book2], [man1])
     book3 = SuBook(name='TestTestTest', author='TestTest', year=1980, id_b=3)
 
     myLib.add_book(book3)
     # myLib.remove_book(3)
 
-    # SuLibrary.print_all_books(myLib)
+    # SuLibrary.print_lib_books(myLib)
 
-    myLib.give_book(3, 1)
+    myLib.give_book(man1, book3)
 
+    SuLibrary.print_lib_books(myLib)
+    myLib.take_book(man1, book3)
+    SuLibrary.print_lib_books(myLib)
