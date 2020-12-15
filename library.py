c77c06545070182eace82ee32b95036be5174a17
NikKrek <35462651+NikKrek@users.noreply.github.com>
2020-12-15 14:27:40 +0200

    Update library.py
    Сhanged variables to lists

--- library.py
+++ library.py
@@ -34,38 +34,38 @@
 
 
 
-class SuBook:
+class SuBook(dict):
 
     def __init__(self, name, author, year, id_b):
-        self.name = name
-        self.author = author
-        self.year = year
-        self.id_b = id_b
+        self["name"] = name
+        self["author"] = author
+        self["year"] = year
+        self["id_b"] = id_b
 
     def __repr__(self):
-        return f'{self.name},{self.author},{self.year},{self.id_b}'
+        return f'{self["name"]},{self["author"]},{self["year"]},{self["id_b"]}'
 
     def __str__(self):
-        return f'{self.name},{self.author},{self.year},{self.id_b}'
+        return f'{self["name"]},{self["author"]},{self["year"]},{self["id_b"]}'
 
 
-class SuLibrary:
-    def __init__(self, list_book: list, man: list):
-        self.list_book = list_book
-        self.man = man
+class SuLibrary(dict):
+    def __init__(self, list_book: list):
+        self["list_book"] = list_book
+        self["man"] = []
 
     def add_book(self, add_book):
-        self.list_book.append(add_book)
+        self["list_book"].append(add_book)
 
     def remove_book(self, id_remove_book: int):
         j = 1
-        for i in self.list_book:
+        for i in self["list_book"]:
             if id_remove_book == j:
-                self.list_book.remove(i)
+                self["list_book"].remove(i)
             j += 1
 
     def print_lib_books(self):
-        book = self.list_book
+        book = self["list_book"]
         print('%40s' % 'Shelf of library')
         print('%-50s%-20s%-5s%-3s' % ('Name:', 'Author', 'Year', 'id'))
         for i in book:
@@ -76,8 +76,10 @@ class SuLibrary:
             print(f'{book_temp[0]:50}{book_temp[1]:20}{book_temp[2]:5}{book_temp[3]:3}')
 
     def give_book(self, mans, books):
-        if books in self.list_book:
-            mans.debt.append(books)
+        if books in self["list_book"]:
+            if mans not in self["man"]:
+                self["man"].append(mans)
+            mans["debt"].append(books)
             books = str(books)
             books = books.split(',')
             j = books[3]
@@ -85,22 +87,34 @@ class SuLibrary:
 
     def take_book(self, mans, books):
         if books in mans.debt:
-            mans.debt.remove(books)
+            mans["debt"].remove(books)
             self.add_book(books)
 
-class SuPeople:
-    def __init__(self, name, surname, id_p, debt=[]):
-        self.name = name
-        self.surname = surname
-        self.id_p = id_p
-        self.debt = debt
+    def print_books_man(self):
+        mans = self["man"]
+        print('%40s' % 'Borrowed books')
+        print('%-50s%-20s%-5s%-3s' % ('Name:', 'Author', 'Year', 'id'))
+        k = ''
+        for i in mans:
+            book_temp = str(i)
+            book_temp = book_temp.replace("[", "")
+            book_temp = book_temp.replace("]", "")
+            book_temp = book_temp.split(',')
+            print(f'{book_temp[0]:50}{book_temp[1]:20}{book_temp[2]:5}{book_temp[3]:3}')
+
+
+class SuPeople(dict):
+    def __init__(self, name, surname, id_p):
+        self["name"] = name
+        self["surname"] = surname
+        self["id_p"] = id_p
+        self["debt"] = []
 
     def __repr__(self):
-        return f'{self.name},{self.surname},{self.id_p},{self.debt}'
+        return f'{self["name"]},{self["surname"]},{self["id_p"]},{self["debt"]}'
 
     def __str__(self):
-        return f'{self.name},{self.surname},{self.id_p},{self.debt}'
-
+        return f'{self["name"]},{self["surname"]},{self["id_p"]},{self["debt"]}'
 
 
 if __name__ == "__main__":
@@ -108,16 +122,16 @@ if __name__ == "__main__":
     book2 = SuBook(name='Charley & chocolate factory ', author='Dahll', year=1970, id_b=2)
 
     man1 = SuPeople(name='Rob', surname='Sapolsky', id_p=1)
-    myLib = SuLibrary([book1, book2], [man1])
+    myLib = SuLibrary([book1, book2])
     book3 = SuBook(name='TestTestTest', author='TestTest', year=1980, id_b=3)
 
     myLib.add_book(book3)
     # myLib.remove_book(3)
 
-    # SuLibrary.print_lib_books(myLib)
-
     myLib.give_book(man1, book3)
 
+    # myLib.take_book(man1, book3)
+    man2 = SuPeople(name='Bob', surname='Ross', id_p=2)
+    myLib.give_book(man2, book2)
     SuLibrary.print_lib_books(myLib)
-    myLib.take_book(man1, book3)
-    SuLibrary.print_lib_books(myLib)
+    SuLibrary.print_books_man(myLib)
